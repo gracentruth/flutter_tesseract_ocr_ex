d93edd7409385427f735861a39ad9508e9d520e3
gracentruth <63465350+gracentruth@users.noreply.github.com>
2021-12-28 23:26:49 +0900

    ocr implement

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,14 @@
+
+
+import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_tesseract_ocr/flutter_tesseract_ocr.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:flutter/services.dart' show ByteData,rootBundle;
 
 void main() {
   runApp(const MyApp());
@@ -44,10 +53,80 @@ class _MyHomePageState extends State<MyHomePage> {
 
   var LangList = ["kor", "eng"];
   var selectList = [];
-  String path = "";
+  String path = "https://raw.githubusercontent.com/khjde1207/tesseract_ocr/master/example/assets/test1.png";
   bool bload = false;
 
   var urlEditController = TextEditingController();
+
+
+
+  void runFilePiker() async { // 사진을 갤러리에서 가지고 오고 싶을 때 실행하는 함수android && ios only
+    final pickedFile =
+    await ImagePicker().getImage(source: ImageSource.gallery);
+    if (pickedFile != null) {
+      _ocr(urlEditController.text);
+    }
+  }
+
+  Future<void> writeToFile(ByteData data, String path) {
+    print('-----');
+    print(data);
+    final buffer = data.buffer;
+    return new File(path).writeAsBytes(
+        buffer.asUint8List(data.offsetInBytes, data.lengthInBytes));
+  }
+
+  void _ocr(url) async {
+    if (selectList.length <= 0) {
+      print("Please select language");
+      return;
+    }
+
+    Directory tempDir = await getTemporaryDirectory();
+    print('tempDir:'+tempDir.toString());
+    HttpClient httpClient = new HttpClient();
+    print('httpClient:'+httpClient.toString());
+    // HttpClientRequest request = await httpClient.getUrl(Uri.parse(url));
+    // print('request:'+request.toString());
+    // HttpClientResponse response = await request.close();
+    // print('resoinse:'+response.toString());
+    // Uint8List bytes = await consolidateHttpClientResponseBytes(response);
+    // print('bytes:'+bytes.toString());
+    String dir = tempDir.path;
+    print('dir:'+dir.toString());
+
+
+    path = url;
+    if (kIsWeb == false &&
+        (url.indexOf("http://") == 0 || url.indexOf("https://") == 0)) {
+
+      Directory tempDir = await getTemporaryDirectory(); //현재의 디렉토리를 받아온다.
+
+      HttpClient httpClient = new HttpClient();
+      HttpClientRequest request = await httpClient.getUrl(Uri.parse(url));
+      HttpClientResponse response = await request.close();
+      Uint8List bytes = await consolidateHttpClientResponseBytes(response);
+      String dir = tempDir.path; //현재 디렉토리의 경로
+      print('$dir/test.jpg');
+      File file = new File('$dir/test.jpg');
+      await file.writeAsBytes(bytes);
+      url = file.path;
+
+    }
+    var langs = selectList.join("+");
+
+    bload = true;
+    setState(() {});
+
+    _ocrText =
+    await FlutterTesseractOcr.extractText(url, language: langs, args: {
+      "preserve_interword_spaces": "1",
+    });
+
+
+    bload = false;
+    setState(() {});
+  }
 
 
   @override
@@ -57,63 +136,97 @@ class _MyHomePageState extends State<MyHomePage> {
 
         title: Text(widget.title),
       ),
-      body: Padding(
-        padding:EdgeInsets.all(10),
-        child:Column(
-          children: [
-           Row(
-             children: [
-               ElevatedButton(
-                 child:Text('urls'),
-                 onPressed:(){
-                   showDialog(
-                       context: context,
-                       builder: (BuildContext context){
-                         return SimpleDialog(
-                           title: const Text('Select Url'),
-
-                         );
-                       });
-
-                 },
-               ),
-               Expanded(
-                 child: TextField(
-                   decoration: InputDecoration(
-                     border: OutlineInputBorder(),
-                     labelText: 'input image url',
-                   ),
-                   controller: urlEditController,
-                 ),
-               ),
-               ElevatedButton(
-                   onPressed: () {
-                     //_ocr(urlEditController.text);
-                   },
-                   child: Text("Run")),
-             ],
-           ),
-            Row(
+      body: Stack(
+        children: [
+          Padding(
+            padding:EdgeInsets.all(10),
+            child:Column(
               children: [
-                ...LangList.map((e){
-                  return Row(
-                    children: [
-                      Checkbox(
-                        value: selectList.indexOf(e) >= 0,
-                        onChanged: (v) {
-                        print('check');
-                        },),
-                      Text(e)
-                    ],
-                  );
-                }).toList(),
+                Row(
+                  children: [
+                    ElevatedButton(
+                      child:Text('urls'),
+                      onPressed:(){
+                        showDialog(
+                            context: context,
+                            builder: (BuildContext context){
+                              return SimpleDialog(
+                                title: const Text('Select Url'),
+
+                              );
+                            });
+
+                      },
+                    ),
+                    Expanded(
+                      child: TextField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'input image url',
+                        ),
+                        controller: urlEditController,
+                      ),
+                    ),
+                    ElevatedButton(
+                        onPressed: () {
+                          _ocr(urlEditController.text);
+                        },
+                        child: Text("Run")),
+                  ],
+                ),
+                Row(
+                  children: [
+                    ...LangList.map((e){
+                      return Row(
+                        children: [
+                          Checkbox(
+                            value: selectList.indexOf(e) >= 0,
+                            onChanged: (v) async{
+                              if (selectList.indexOf(e) < 0) {
+                                selectList.add(e);
+                              } else {
+                                selectList.remove(e);
+                              }
+                              setState(() {});
+                            },),
+                          Text(e)
+                        ],
+                      );
+                    }).toList(),
+                  ],
+                ),
+                Expanded( //사진 보여주는 부분
+                    child: ListView(
+                      children: [
+                        path.length <= 0
+                            ? Container()
+                            : path.indexOf("http") >= 0
+                            ? Image.network(path)
+                            : Image.file(File(path)),
+                        bload
+                            ? Column(children: [CircularProgressIndicator()])
+                            : Text(
+                          '$_ocrText', // 찾아낸 문자열 내용들
+                        ),
+                      ],
+                    ))
+
               ],
+
             ),
+          ),
 
-          ],
 
-        ),
+        ],
       ),
+    //  floatingActionButton: FloatingActionButton(
+    //     onPressed: () {
+    //   runFilePiker();
+    //   _ocr();
+    // },
+    // tooltip: 'OCR',
+    // child: Icon(Icons.add),
+    // ), // Th,
 
     );
   }
